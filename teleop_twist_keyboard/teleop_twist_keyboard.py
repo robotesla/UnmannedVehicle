80e6d0bb601cb2261efbbd9d5f028aafba600250
Евгений Александрович <volkov@robotesla.ru>
2020-12-12 20:35:21 +0300

    адаптация teleop под модель

--- teleop_twist_keyboard/teleop_twist_keyboard.py
+++ teleop_twist_keyboard/teleop_twist_keyboard.py
@@ -7,7 +7,7 @@ import threading
 import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
 
-from geometry_msgs.msg import Twist
+from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
 
 import sys, select, termios, tty
 
@@ -19,20 +19,10 @@ Moving around:
    j    k    l
    m    ,    .
 
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
-
-t : up (+z)
-b : down (-z)
 
 anything else : stop
 
 q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
 
 CTRL-C to quit
 """
@@ -46,34 +36,18 @@ moveBindings = {
         ',':(-1,0,0,0),
         '.':(-1,0,0,1),
         'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
     }
 
 speedBindings={
-        'q':(1.1,1.1),
-        'z':(.9,.9),
-        'w':(1.1,1),
-        'x':(.9,1),
-        'e':(1,1.1),
-        'c':(1,.9),
+        'q':(1.05,1.01),
+        'z':(.95,.09),
     }
 
 class PublishThread(threading.Thread):
     def __init__(self, rate):
         super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        self.publisher = rospy.Publisher('/uv/ackermann_cmd', AckermannDriveStamped, queue_size = 1)
         self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
         self.th = 0.0
         self.speed = 0.0
         self.turn = 0.0
@@ -100,11 +74,9 @@ class PublishThread(threading.Thread):
         if rospy.is_shutdown():
             raise Exception("Got shutdown request before subscribers connected")
 
-    def update(self, x, y, z, th, speed, turn):
+    def update(self, x, th, speed, turn):
         self.condition.acquire()
         self.x = x
-        self.y = y
-        self.z = z
         self.th = th
         self.speed = speed
         self.turn = turn
@@ -114,23 +86,20 @@ class PublishThread(threading.Thread):
 
     def stop(self):
         self.done = True
-        self.update(0, 0, 0, 0, 0, 0)
+        self.update(0, 0, 0, 0)
         self.join()
 
     def run(self):
-        twist = Twist()
+        twist = AckermannDriveStamped()
+        drive = AckermannDrive()
         while not self.done:
             self.condition.acquire()
             # Wait for a new message or timeout.
             self.condition.wait(self.timeout)
 
             # Copy state into twist message.
-            twist.linear.x = self.x * self.speed
-            twist.linear.y = self.y * self.speed
-            twist.linear.z = self.z * self.speed
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = self.th * self.turn
+            twist.drive.speed = self.x * self.speed
+            twist.drive.steering_angle = self.th * self.turn
 
             self.condition.release()
 
@@ -138,12 +107,8 @@ class PublishThread(threading.Thread):
             self.publisher.publish(twist)
 
         # Publish stop message when thread exits.
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = 0
+        twist.drive.speed = 0
+        twist.drive.steering_angle = 0
         self.publisher.publish(twist)
 
 
@@ -166,8 +131,8 @@ if __name__=="__main__":
 
     rospy.init_node('teleop_twist_keyboard')
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
+    speed = rospy.get_param("~speed", 1)
+    turn = rospy.get_param("~turn", 10.0)
     repeat = rospy.get_param("~repeat_rate", 0.0)
     key_timeout = rospy.get_param("~key_timeout", 0.0)
     if key_timeout == 0.0:
@@ -176,14 +141,12 @@ if __name__=="__main__":
     pub_thread = PublishThread(repeat)
 
     x = 0
-    y = 0
-    z = 0
     th = 0
     status = 0
 
     try:
         pub_thread.wait_for_subscribers()
-        pub_thread.update(x, y, z, th, speed, turn)
+        pub_thread.update(x, th, speed, turn)
 
         print(msg)
         print(vels(speed,turn))
@@ -191,8 +154,6 @@ if __name__=="__main__":
             key = getKey(key_timeout)
             if key in moveBindings.keys():
                 x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
                 th = moveBindings[key][3]
             elif key in speedBindings.keys():
                 speed = speed * speedBindings[key][0]
@@ -205,16 +166,14 @@ if __name__=="__main__":
             else:
                 # Skip updating cmd_vel if key timeout and robot already
                 # stopped.
-                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+                if key == '' and x == 0 and th == 0:
                     continue
                 x = 0
-                y = 0
-                z = 0
                 th = 0
                 if (key == '\x03'):
                     break
  
-            pub_thread.update(x, y, z, th, speed, turn)
+            pub_thread.update(x, th, speed, turn)
 
     except Exception as e:
         print(e)
